835e4013490a54756aaab31fe91d268f56cf3a26
Vladyslav Chyzhevskyi <coirius@coirius.com>
2013-04-01 19:14:43 +0300

    renamed Get-AmazonS3Buckets to List-AmazonS3Buckets; added List-AmazonS3BucketObjects cmdlet

--- AWS.ps1
+++ AWS.ps1
@@ -1,6 +1,6 @@
 ﻿[Reflection.Assembly]::LoadWithPartialName("System.Xml.Linq") | Out-Null
 
-function Get-AmazonS3Buckets ($AccessKeyId, $SecretAccessKey) {
+function List-AmazonS3Buckets ($AccessKeyId, $SecretAccessKey) {
     $timeStamp = [System.DateTime]::UtcNow;
     $strToSign = [System.String]::Format("GET`n`n`n{0}`n{1}", $timeStamp.ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'"), "/");
     $utf8 = New-Object System.Text.UTF8Encoding;
@@ -14,7 +14,32 @@ function Get-AmazonS3Buckets ($AccessKeyId, $SecretAccessKey) {
             $obj = New-Object psobject -Property @{
                 Name = $xmlObj.Name;
                 CreationDate = [System.DateTime]::Parse($xmlObj.CreationDate);
-            }
+            };
+            $obj;
+        }
+    }
+    catch [System.Net.WebException] {
+        $respStreamReader = New-Object System.IO.StreamReader -arg $_.Exception.Response.GetResponseStream();
+        Write-Host $respStreamReader.ReadToEnd();
+    }
+}
+
+function List-AmazonS3BucketObjects ($AccessKeyId, $SecretAccessKey, $Bucket) {
+    $timeStamp = [System.DateTime]::UtcNow;
+    $strToSign = [System.String]::Format("GET`n`n`n{0}`n/{1}/", $timeStamp.ToString("ddd, dd MMM yyyy HH:mm:ss 'GMT'"), $Bucket);
+    $utf8 = New-Object System.Text.UTF8Encoding;
+    $hasher = New-Object System.Security.Cryptography.HMACSHA1;
+    $hasher.Key = $utf8.GetBytes($SecretAccessKey);
+    $req = [System.Net.HttpWebRequest][System.Net.WebRequest]::Create([System.String]::Format("http://{0}.s3.amazonaws.com/", $Bucket));
+    $req.Headers.Add("Authorization", [System.String]::Format("AWS {0}:{1}", $AccessKeyId, [System.Convert]::ToBase64String($hasher.ComputeHash($utf8.GetBytes($strToSign)))));
+    ([System.Net.HttpWebRequest]$req).Date = $timeStamp;
+    try {
+        foreach($xmlObj in ([xml](New-Object System.IO.StreamReader -arg ([System.Net.HttpWebRequest]$req).GetResponse().GetResponseStream()).ReadToEnd()).ListBucketResult.Contents) {
+            $obj = New-Object psobject -Property @{
+                Key = $xmlObj.Key;
+                Size = $xmlObj.Size;
+                LastModified = [System.DateTime]::Parse($xmlObj.LastModified);
+            };
             $obj;
         }
     }
